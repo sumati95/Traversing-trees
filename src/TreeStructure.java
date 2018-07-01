040934e8157dd83fba808f708cc061f04818dd37
Sumati <kumarisumati95@gmail.com>
2018-07-01 18:07:04 +0530

    some tree codes

---
+++ src/TreeStructure.java
@@ -0,0 +1,56 @@
+
+public class TreeStructure {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.left.right = new TreeNode(5);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.left.right = new TreeNode(9);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.right.right.right = new TreeNode(10);
+        preOrder(root);
+        System.out.println(sumTree(root));
+    }
+
+    static void preOrder(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+    }
+
+    static void postOrder(TreeNode root) {
+        if (root != null) {
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.println(root.data + " ");
+        }
+    }
+
+    static void inOrder(TreeNode root) {
+        if (root != null) {
+            inOrder(root.left);
+            System.out.println(root.data + " ");
+            inOrder(root.left);
+        }
+    }
+
+    static int addNodes(TreeNode root) {
+        int sum;
+        if (root != null) {
+            
+        }
+    }
+}
+
+class TreeNode {
+    int data;
+    TreeNode left, right;
+    TreeNode(int d) {
+        data = d;
+    }
+}
